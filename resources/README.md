c2bcc0c8c08ab734eda0c753e0f591d0b02e17e5
Sam Foster <sfoster@mozilla.com>
2019-06-16 13:28:48 -0700

    More work on positioning, factor out health, radial and creep components. Add in walking zombie

--- resources/README.md
+++ resources/README.md
@@ -1,8 +1,10 @@
 zed-gunner Resources
 ====================
 
-Image Credits:
+Art Credits:
 
-* [imgp5525_seamless.jpg] (https://opengameart.org/node/10180)
+* [imgp5525_seamless.jpg](https://opengameart.org/node/10180)
   - CC licensed, author: [arikel](https://opengameart.org/users/arikel)
+* [squid.gltf/squid.glb](https://sketchfab.com/3d-models/squid-npc-4a308772eb8c4ea5a675cc35d9d99070)
+  - CC licensed, author: [Xillute | Dev](https://sketchfab.com/janchristian)
 
---
+++ src/public/assets/components.js
@@ -0,0 +1,131 @@
+const TAU = Math.PI+Math.PI;
+const QUARTER_CIRCLE = Math.PI/2;
+const ONE_DEGREE = Math.PI/180;
+
+AFRAME.registerComponent("health", {
+  schema: {type: 'int', default: 0},
+  update(oldData) {
+    if (this.data.health <= 0 && oldData.health > 0) {
+      console.log("health component update, ", this.el, this.data.health);
+      this.emit("death");
+    }
+  }
+});
+
+/*
+ * position an entity at a distance and angle from a point
+ */
+AFRAME.registerComponent("radial", {
+  schema: {
+    angleDegrees: { type: 'number', default: -1 }, // degrees
+    radialDistance: { type: 'number', default: -1 },
+  },
+  play() {
+    console.log(this.el.id, "radial#play at: ", this.data.radialDistance);
+  },
+  update(oldData) {
+    if (this.data.angleDegrees == -1) {
+      throw new Error("radial component never got an angleDegrees?");
+    }
+    if (oldData.angleDegrees !== this.data.angleDegrees) {
+      let rotateY = Math.PI * 2.5 - this.data.angleDegrees * ONE_DEGREE; // correct model rotation e.g. by  - QUARTER_CIRCLE
+      console.log("adjust rotation: ", rotateY);
+      this.el.object3D.rotation.set(0,
+                                    rotateY,
+                                    0);
+    }
+    this.updatePosition(this.data.angleDegrees * ONE_DEGREE, this.data.radialDistance);
+  },
+  updatePosition(radians, radius) {
+    this.el.object3D.position.x = Math.cos(radians) * radius;
+    this.el.object3D.position.y = 1;
+    this.el.object3D.position.z = Math.sin(radians) * radius;
+  },
+});
+
+AFRAME.registerComponent("creep", {
+  schema: {
+    speed: { type: 'number', default: 0.75 }, // metre/second
+    finishDistance: { type: 'number', default: 1 },
+  },
+  healthColors: [
+      "#ff0000",
+      "#b31b1b", // 1: critical
+      "#4b5320", // 2: healthy
+  ],
+  init() {
+    console.log(this.el.id, "creep#init");
+  },
+  play() {
+    console.log(this.el.id, "creep#play");
+    this.el.addEventListener("click", this);
+  },
+  pause() {
+    console.log(this.el.id, "creep#pause");
+    this.el.removeEventListener("click", this);
+  },
+  handleEvent(evt) {
+    if (evt.type == "click") {
+      console.log(this.el.id, "handle click event", this.el.isPlaying, this.el.object3D.visible, this.el.is("dead"));
+      if (!this.el.isPlaying) {
+        throw new Error("Got click on paused entity");
+      }
+      if (!this.el.object3D.visible) {
+        throw new Error("Got click invisible entity");
+      }
+      this.takeHit();
+    }
+  },
+  resetToDefaults() {
+    for (let name in this.schema) {
+      this.data[name] = this.schema[name].default;
+    }
+  },
+  remove() {
+    // set everything back to defaults
+    this.resetToDefaults();
+  },
+  getRadius() {
+    return this.el.components.radial.data.radialDistance;
+  },
+  tick: (function() {
+    let radius;
+    return function(time, timeDelta) {
+      // update position
+      if (!this.el.isConnected) return; // does this ever happen?
+      if (this.el.is("dead")) return;
+
+      radius = this.getRadius();
+      if (radius < 0) return;
+      if (radius > this.data.finishDistance) {
+        this.el.setAttribute("radial", "radialDistance", radius - this.data.speed * .001 * timeDelta);
+      } else {
+        // maybe do damage
+        // then die
+        console.log("do damage: ", this.el.components.health.data);
+        this.el.emit("attack", this.el.components.health.data);
+        this.die();
+      }
+    };
+  })(),
+  takeHit() {
+    console.log("takeHit!", this.el.components);
+    let health = this.el.components.health.data -1;
+    this.el.setAttribute("health", health);
+
+    if (health > 0) {
+      // color by health
+      this.el.setAttribute('material', 'color', this.healthColors[health]);
+      // knock it back a bit
+      this.el.setAttribute("radial", "radialDistance", this.getRadius() + 1);
+    } else {
+      this.die();
+    }
+    // console.log('I was clicked at: ', evt.detail.intersection.point);
+  },
+  die() {
+    this.resetToDefaults();
+    this.el.addState("dead");
+  },
+});
+
